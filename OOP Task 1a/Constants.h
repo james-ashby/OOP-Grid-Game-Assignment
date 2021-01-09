4847457374cefac1e792e80efb95144cd1922baf
James Ashby <jamesashby1@hotmail.co.uk>
2021-01-09 10:23:02 +0000

    Added TODO UI bug fixes

--- OOP Task 1a/Constants.h
+++ OOP Task 1a/Constants.h
@@ -18,15 +18,18 @@
 
 #define MAP1 {\
 {\
-    { 'F','F','F','F','F','F','F','F','F','F' },\
+    { 'F','F','F','F','W','F','F','F','F','F' },\
     { 'F','F','F','F','F','F','F','F','F','F' },\
     { 'F','F','F','W','W','W','F','F','F','H' },\
     { 'F','F','F','W','F','W','F','F','F','H' },\
-    { 'F','F','F','W','F','W','W','F','F','F' },\
+    { 'W','F','F','W','F','W','W','F','F','F' },\
     { 'F','F','F','W','F','F','F','F','F','F' },\
     { 'F','F','F','W','W','W','W','F','F','F' },\
     { 'F','F','F','F','F','F','F','F','F','F' },\
     { 'F','F','P','F','F','F','F','F','F','F' },\
-    { 'F','F','F','F','F','F','F','F','F','F' }\
+    { 'F','F','F','F','F','W','F','F','F','F' }\
 }\
               }
+
+// TODO : UI Grid is missing left coloumn, top row
+// TODO : UI Grid is showing extra right column, extra bottom row

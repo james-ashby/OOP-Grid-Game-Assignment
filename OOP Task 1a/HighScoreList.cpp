005fad5543ad226d776a456441a65980b51b2ab1
CozJ <corey.j.sutcliffe@gmail.com>
2021-01-16 14:26:33 +0000

    removed comment

--- OOP Task 1a/HighScoreList.cpp
+++ OOP Task 1a/HighScoreList.cpp
@@ -10,7 +10,7 @@ vector<int> HighScoreList::GetHighScoreList()
     highScoreFile.open("HighScores.txt");
     if (highScoreFile.is_open())
     {
-        while (getline(highScoreFile,line)) // not a clue why this wont work, trying to google issue just gives 1000s pages of include string... it is included.
+        while (getline(highScoreFile,line))
         {
             highScores.push_back(stoi(line));
         }

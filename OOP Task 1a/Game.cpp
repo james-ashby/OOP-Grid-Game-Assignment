9e2a3fb93ce6bd81bbcb6802772462e7634b43db
Corey <corey.j.sutcliffe@gmail.com>
2021-01-20 13:35:23 +0000

    high scores almost done

--- OOP Task 1a/Game.cpp
+++ OOP Task 1a/Game.cpp
@@ -174,8 +174,15 @@ bool Game::IsRunning()
 
 void Game::ChangeLevel()
 {
-    currentLevel++;
-    player.MoveToSpawn();
+    if (currentLevel < 4)
+    {
+        currentLevel++;
+        player.MoveToSpawn();
+    }
+    else
+    {
+        started = false;
+    }
 }
 
 void Game::LevelRemoveKey()
